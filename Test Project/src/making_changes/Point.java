f1ac11ac2bc64fcface15a5668d3bfae3fd2590c
MACBOOK <zkunitsa@yahoo.com>
2018-10-02 00:08:30 -0700

    Added getters and setters and a to string method to the point class

--- Test Project/src/making_changes/Point.java
+++ Test Project/src/making_changes/Point.java
@@ -9,4 +9,28 @@ public class Point {
         this.x = x;
         this.y = y;
     }
+
+    public Double getX() {
+        return x;
+    }
+
+    public void setX(Double x) {
+        this.x = x;
+    }
+
+    public Double getY() {
+        return y;
+    }
+
+    public void setY(Double y) {
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
 }
