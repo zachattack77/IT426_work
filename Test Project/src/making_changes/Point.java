6202bef66481f532f417019e8cfbbe3eaef77d63
MACBOOK <zkunitsa@yahoo.com>
2018-10-02 00:27:42 -0700

    Rvereted to a previous of my point class. comitting changes to alter my x,y coordinates to x,y,z coordinates

--- Test Project/src/making_changes/Point.java
+++ Test Project/src/making_changes/Point.java
@@ -1,46 +1,28 @@
 package making_changes;
 
-import java.util.Objects;
-
 public class Point {
 
     private Double x;
     private Double y;
+    private Double z;
 
-    public Point(Double x, Double y) {
+    public Point(Double x, Double y, Double z) {
         this.x = x;
         this.y = y;
+        this.z = z;
     }
 
     public Double getX() {
         return x;
     }
 
-    public void setX(Double x) {
-        this.x = x;
-    }
 
     public Double getY() {
         return y;
     }
 
-    public void setY(Double y) {
-        this.y = y;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Point point = (Point) o;
-        return Objects.equals(x, point.x) &&
-                Objects.equals(y, point.y);
-    }
-
-    @Override
-    public int hashCode() {
-
-        return Objects.hash(x, y);
+    public Double getZ() {
+        return z;
     }
 
     @Override
@@ -48,6 +30,7 @@ public class Point {
         return "Point{" +
                 "x=" + x +
                 ", y=" + y +
+                ", z=" + z +
                 '}';
     }
 }
