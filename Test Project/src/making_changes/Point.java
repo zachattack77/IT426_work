fc0d966f1c91ec32d23aa4ebc343353c81cbc6e8
MACBOOK <zkunitsa@yahoo.com>
2018-10-02 00:05:27 -0700

    Added a point class to represent x, y coordinates

---
+++ Test Project/src/making_changes/Point.java
@@ -0,0 +1,12 @@
+package making_changes;
+
+public class Point {
+
+    private Double x;
+    private Double y;
+
+    public Point(Double x, Double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
